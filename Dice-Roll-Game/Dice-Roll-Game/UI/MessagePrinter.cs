4ed0ab0a0ebf482c147c97765b1a7357e2948e63
AbbasZandi97 <abbas.zandi97@gmail.com>
2026-06-26 00:37:01 +0330

    MessagePrinter.cs: Complete the class

--- Dice-Roll-Game/Dice-Roll-Game/UI/MessagePrinter.cs
+++ Dice-Roll-Game/Dice-Roll-Game/UI/MessagePrinter.cs
@@ -1,10 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Dice_Roll_Game.UI
+﻿namespace Dice_Roll_Game.UI
 {
-    internal class MessagePrinter
+    // This class is responsible for all console output
+    static class MessagePrinter
     {
+        public static void Welcome()
+        {
+            Console.WriteLine("*************** Dice Roll Game ***************");
+            Console.WriteLine("Dice rolled. Guess what number it shows in 3 tries.");
+            Console.WriteLine("====================================================");
+        }
+
+        public static void GuessTheNumber()
+        {
+            Console.WriteLine("Please enter your guess: ");
+        }
+
+        public static void PrintInvalidInput()
+        {
+            Console.WriteLine("Input is invalid. Enter a valid number.");
+        }
+
+        public static void PrintWrongNumber()
+        {
+            Console.WriteLine("Wrong number. 😢");
+        }
+
+        public static void PrintWin()
+        {
+            Console.WriteLine("You win ! 😁");
+        }
+
+        public static void PrintLose()
+        {
+            Console.WriteLine("You lose. 😔");
+        }
+
+        public static void PrintFinishGame()
+        {
+            Console.WriteLine("*************** The Game is finished ***************");
+        }
     }
 }

e79b43d406c3a022b5c8f4febd24f07642b55f92
AbbasZandi97 <abbas.zandi97@gmail.com>
2026-06-26 00:36:15 +0330

    InputHandler.cs: Complete the class

--- Dice-Roll-Game/Dice-Roll-Game/UI/InputHandler.cs
+++ Dice-Roll-Game/Dice-Roll-Game/UI/InputHandler.cs
@@ -1,10 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Dice_Roll_Game.UI
+﻿namespace Dice_Roll_Game.UI
 {
+    // The class is responsible for reading and validating user input
     internal class InputHandler
     {
+        public static int GetInput()
+        {
+            string input;
+            do
+            {
+                MessagePrinter.GuessTheNumber();
+                input = Console.ReadLine();
+            }
+            while (!IsInputValid(input));
+
+            return int.Parse(input);
+
+        }
+
+        public static bool IsInputValid(string input)
+        {
+            bool isValid = int.TryParse(input, out _);
+
+            if (!isValid) MessagePrinter.PrintInvalidInput();
+
+            return isValid;
+        }
+
     }
 }

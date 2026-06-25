f075a5d0f7cb898f21e24fe7de15cb6797ab24c6
AbbasZandi97 <abbas.zandi97@gmail.com>
2026-06-26 00:34:01 +0330

    Program.cs: Complete the class.

--- Dice-Roll-Game/Dice-Roll-Game/Program.cs
+++ Dice-Roll-Game/Dice-Roll-Game/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Dice_Roll_Game
+﻿using Dice_Roll_Game.Services;
+using System.Text;
+namespace Dice_Roll_Game
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // this line makes sure emojis in the code are shown perfectly on the console
+            Console.OutputEncoding = Encoding.UTF8;
+           
+            new GameService().StartGame();
         }
     }
 }
